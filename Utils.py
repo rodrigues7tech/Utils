3b1117d926f8789798bd3c4cb89e64201600e068
Gilberto Rodrigues Santos <gilberto.rodrigues@br.gt.com>
2023-12-26 11:57:11 -0300

    Add Descompactar arquivos zip

--- Utils.py
+++ Utils.py
@@ -1,4 +1,7 @@
 import re
+import os
+import zipfile
+
 
 def formatar_cpf(cpf):
     cpf_numerico = re.sub(r'\D', '', cpf)
@@ -30,4 +33,21 @@ def formatar_cnj(cnj):
     
     return cnj_formatado
 
+def descompactar_arquivo_zip(diretorio: str):
+    list_arquivos_zip = []
+    for arquivo in os.listdir(diretorio):
+        if arquivo.endswith('.zip'):
+            list_arquivos_zip.append(arquivo)
 
+    if len(list_arquivos_zip) > 0:
+        for arquivo_zip in list_arquivos_zip:
+            caminho_arquivo_zip = os.path.join(diretorio, arquivo_zip)
+
+            with zipfile.ZipFile(caminho_arquivo_zip, 'r') as zip_ref:
+                pasta_destino = os.path.join(diretorio, arquivo_zip.split('.')[0])
+                os.makedirs(pasta_destino, exist_ok=True)
+                zip_ref.extractall(pasta_destino)
+            
+            os.unlink(caminho_arquivo_zip)
+    else:
+        raise Exception('Nenhum arquivo .zip foi encontrado!')
