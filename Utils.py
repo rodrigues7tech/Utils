afa15471f464b390715e2d1f4d976aed438ff45b
Gilberto Rodrigues <rodrigues.tech@hotmail.com>
2023-12-25 23:34:33 -0300

    Add formatar CNPJ

--- Utils.py
+++ Utils.py
@@ -1,15 +1,24 @@
 import re
 
 def formatar_cpf(cpf):
-    # Remover caracteres não numéricos
     cpf_numerico = re.sub(r'\D', '', cpf)
 
-    # Verificar se o CPF possui 11 dígitos
     if len(cpf_numerico) != 11:
         raise ValueError("CPF deve conter 11 dígitos")
 
     cpf_formatado = re.sub(r'(\d{3})(\d{3})(\d{3})(\d{2})', r'\1.\2.\3-\4', cpf_numerico)
     
     return cpf_formatado
+
+def formatar_cnpj(cnpj):
+    cnpj_numerico = re.sub(r'\D', '', cnpj)
+
+    if len(cnpj_numerico) != 14:
+        raise ValueError("CNPJ deve conter 14 dígitos")
+
+    cnpj_formatado = re.sub(r'(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})', r'\1.\2.\3/\4-\5', cnpj_numerico)
+    
+    return cnpj_formatado
+
 
 
